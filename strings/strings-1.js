84de24008703889f73a5e4136596df8a51d78670
Nicolas Canda <nicolasacanda@gmail.com>
2023-03-01 18:33:15 -0300

    resolviendo primer ejercicio de strings - 1

--- strings/strings-1.js
+++ strings/strings-1.js
@@ -1,11 +1,11 @@
-const myString = 'Pepe';
+const myString = "timoteo";
 
-console.log('myString: ', myString);
-console.log('Longitud: ', myString.length);
-console.log('Primer Letra: ', myString[0]);
-console.log('Ultima letra: ', myString[myString.length - 1]);
-console.log('Mayúsculas: ', myString.toUpperCase());
-console.log('Minúsculas: ', myString.toLowerCase());
+// console.log("myString: ", myString);
+// console.log("Longitud: ", myString.length);
+// console.log("Primer Letra: ", myString[0]);
+// console.log("Ultima letra: ", myString[myString.length - 1]);
+// console.log("Mayúsculas: ", myString.toUpperCase());
+// console.log("Minúsculas: ", myString.toLowerCase());
 
 // ----
 // Crea una funcion, que tome cualqier nombre, y siempre te devuelva
@@ -13,3 +13,12 @@ console.log('Minúsculas: ', myString.toLowerCase());
 // que ha de ser en mayúsculas.
 
 // ----
+
+const firstLetterUpperCase = (text) => {
+  const firstLetter = text[0].toUpperCase();
+  const restOfTheText = text.slice(1, text.length).toLowerCase();
+
+  return firstLetter + restOfTheText;
+};
+
+console.log(firstLetterUpperCase(myString));
