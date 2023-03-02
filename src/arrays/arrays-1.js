a9a6f53e07225842d455a382be8a79d3c0d83040
Sergio Díaz <diaz.sergio@protonmail.com>
2023-03-02 20:08:42 -0300

    updating

---
+++ src/arrays/arrays-1.js
@@ -0,0 +1,12 @@
+// const myString = 'Un String';
+
+// console.log('myString: ', myString[0]);
+
+const unasFrutas = ['pera', 'manzana'];
+const otrasFrutas = ['manana', 'banana'];
+
+const unaFruta = 'cereza';
+
+const algunaDeEstas = unasFrutas.includes('sandía');
+
+console.log('algunaDeEstas: ', algunaDeEstas);
---
+++ src/test/test.js
@@ -0,0 +1,16 @@
+const num = 1;
+
+num > 1 && console.log("mayor");
+
+if (num > 1) console.log("mayor");
+
+if (num < 10) {
+  console.log("menor");
+  // mucho codico
+  // mucho codico
+  // mucho codico
+  // mucho codico
+  // mucho codico
+  // mucho codico
+  // mucho codico
+}
