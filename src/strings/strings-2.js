72222c321f63585af3c7d7dd4a6829ae7c57f30d
Sergio Díaz <diaz.sergio@protonmail.com>
2023-03-02 14:34:49 -0300

    Añadiendo archivo strings-2

---
+++ src/strings/strings-2.js
@@ -0,0 +1,39 @@
+// Crea una función que tome un string y un número como argumento, y devuelva true si el string es igual o más largo
+// que el número, y false si no.
+// Ejemplo: tuFunción("Hola Mundo", 5); // true
+// Ejemplo: tuFunción("Hola Mundo", 10); // false
+
+// ----
+
+// Crea una función que tome un string como argumento y reemplace todas las ocurrencias de un carácter específico por otro.
+// Utilizar el método replace()
+// Ejemplo: tuFunción("Hola Mundo", "o", "0"); // "H0la Mund0"
+
+// ----
+
+// Crea una función que tome un string como argumento y devuelva un substring de ese string.
+// Utilizar el método substring()
+// Ejemplo: tuFunción("Hola Mundo", 2, 5); // "la M"
+
+// ----
+
+// Crea una función que tome dos strings como argumentos y devuelva true si el segundo string existe en el primero.
+// Utilizar el método includes()
+// Ejemplo: tuFunción("Hola Mundo", "Mundo"); // true
+// Ejemplo: tuFunción("Hola Mundo", "manzana"); // false
+
+// ----
+
+// Crea una función que tome un string y lo convierta en una ruta de navegación, es decir, que reemplace los espacios por guiones.
+// y añada una barra al principio, y minusculas todo.
+// Ejemplo: tuFunción("Hola Mundo"); // "/hola-mundo"
+// Ejemplo: tuFunción("products list"); // "/products-list"
+
+// ----
+
+// Crea una función que tome un nombre de usuario, que pueda estar mal escrito, y lo devuelva correctamente formateado.
+// Debe corregir las mayúsculas y minúsculas, quitar los espacios al principio y al final,
+// quitar puntos si los hubiera
+// Ejemplo: tuFunción("    pePe ferNanDez.    "); // "Pepe Fernandez"
+
+// ----
