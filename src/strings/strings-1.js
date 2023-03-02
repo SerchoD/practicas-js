1c0601bf86a870d35363285cfdeae6ffb007c75b
Sergio Díaz <diaz.sergio@protonmail.com>
2023-03-01 22:31:01 -0300

    corrigiendo espacio del tab 2

--- src/strings/strings-1.js
+++ src/strings/strings-1.js
@@ -1,4 +1,4 @@
-const myString = 'timoteo';
+const myString = "timoteo";
 
 // console.log("myString: ", myString);
 // console.log("Longitud: ", myString.length);
@@ -13,10 +13,10 @@ const myString = 'timoteo';
 // que ha de ser en mayúsculas.
 
 const firstLetterUpperCase = (value) => {
-	const firstLetter = value[0].toUpperCase();
-	const restOfTheText = value.slice(1, value.length).toLowerCase();
+  const firstLetter = value[0].toUpperCase();
+  const restOfTheText = value.slice(1, value.length).toLowerCase();
 
-	return firstLetter + restOfTheText;
+  return firstLetter + restOfTheText;
 };
 
 // console.log(firstLetterUpperCase(myString));
