42ea1e8de1718ac0be6730eb660be5297866216d
Sergio Díaz <diaz.sergio@protonmail.com>
2023-03-02 17:39:20 -0300

    creado carpeta de bucles y archivo loopFor

---
+++ src/loops/loopFor.js
@@ -0,0 +1,17 @@
+// for (inicialización; condición; actualización) {
+//sentencias a ejecutar en cada iteración
+// }
+
+const myString = 'Lata';
+let response;
+
+for (let i = 0; i < myString.length; i++) {
+	if (myString[i] === 'a' || myString[i] === 'A') {
+		response = true;
+		break;
+	} else {
+		response = false;
+	}
+}
+
+console.log(`el string "${myString}" tiene a ???: `, response);
